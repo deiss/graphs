4c1198897e6c04c3aa5882e097503f3bf83eb259
Olivier Deiss <olivier.deiss@gmail.com>
2015-12-10 11:42:24 +0100

    new constants

--- Constants.cpp
+++ Constants.cpp
@@ -2,31 +2,31 @@
 
 namespace Constants {
 
-    unsigned char EDGE_ALGO_RESULT_COLOR_R(255);
-    unsigned char EDGE_ALGO_RESULT_COLOR_G(0);
-    unsigned char EDGE_ALGO_RESULT_COLOR_B(0);
-    unsigned char EDGE_ALGO_SOURCE_COLOR_R(255);
-    unsigned char EDGE_ALGO_SOURCE_COLOR_G(0);
-    unsigned char EDGE_ALGO_SOURCE_COLOR_B(255);
-    unsigned char EDGE_ALGO_DESTINATION_COLOR_R(255);
-    unsigned char EDGE_ALGO_DESTINATION_COLOR_G(150);
-    unsigned char EDGE_ALGO_DESTINATION_COLOR_B(150);
-    unsigned char EDGE_COLOR_R(0);
-    unsigned char EDGE_COLOR_G(0);
-    unsigned char EDGE_COLOR_B(140);
-    int           EDGE_MAXIMUM_CAPACITY(10);
-    int           EDGE_DEFAULT_CAPACITY(0);
-    int           GRAPH_MINIMUM_VERTICES(10);
-    int           GRAPH_NB_VERTICES_TRAVELING_SALESMAN(4);
-    int           GRAPH_VERTICES_INCREMENTATION(70);
-    unsigned char VERTEX_COLOR_R(130);
-    unsigned char VERTEX_COLOR_G(255);
-    unsigned char VERTEX_COLOR_B(180);
-    unsigned char VERTEX_PATH_COLOR_R(200);
-    unsigned char VERTEX_PATH_COLOR_G(0);
-    unsigned char VERTEX_PATH_COLOR_B(0);
-    unsigned char VERTEX_VISITED_COLOR_R(200);
-    unsigned char VERTEX_VISITED_COLOR_G(255);
-    unsigned char VERTEX_VISITED_COLOR_B(0);
+    const unsigned char EDGE_ALGO_RESULT_COLOR_R(255);
+    const unsigned char EDGE_ALGO_RESULT_COLOR_G(0);
+    const unsigned char EDGE_ALGO_RESULT_COLOR_B(0);
+    const unsigned char EDGE_ALGO_SOURCE_COLOR_R(255);
+    const unsigned char EDGE_ALGO_SOURCE_COLOR_G(0);
+    const unsigned char EDGE_ALGO_SOURCE_COLOR_B(255);
+    const unsigned char EDGE_ALGO_DESTINATION_COLOR_R(255);
+    const unsigned char EDGE_ALGO_DESTINATION_COLOR_G(150);
+    const unsigned char EDGE_ALGO_DESTINATION_COLOR_B(150);
+    const unsigned char EDGE_COLOR_R(0);
+    const unsigned char EDGE_COLOR_G(0);
+    const unsigned char EDGE_COLOR_B(140);
+    const int           EDGE_MAXIMUM_CAPACITY(10);
+    const int           EDGE_DEFAULT_CAPACITY(0);
+    const int           GRAPH_MINIMUM_VERTICES(10);
+    const int           GRAPH_NB_VERTICES_TRAVELING_SALESMAN(6);
+    const int           GRAPH_VERTICES_INCREMENTATION(70);
+    const unsigned char VERTEX_COLOR_R(130);
+    const unsigned char VERTEX_COLOR_G(255);
+    const unsigned char VERTEX_COLOR_B(180);
+    const unsigned char VERTEX_PATH_COLOR_R(200);
+    const unsigned char VERTEX_PATH_COLOR_G(0);
+    const unsigned char VERTEX_PATH_COLOR_B(0);
+    const unsigned char VERTEX_VISITED_COLOR_R(200);
+    const unsigned char VERTEX_VISITED_COLOR_G(255);
+    const unsigned char VERTEX_VISITED_COLOR_B(0);
 
 }
--- Constants.hpp
+++ Constants.hpp
@@ -3,32 +3,32 @@
 
 namespace Constants {
 
-    extern unsigned char EDGE_ALGO_RESULT_COLOR_R;
-    extern unsigned char EDGE_ALGO_RESULT_COLOR_G;
-    extern unsigned char EDGE_ALGO_RESULT_COLOR_B;
-    extern unsigned char EDGE_ALGO_SOURCE_COLOR_R;
-    extern unsigned char EDGE_ALGO_SOURCE_COLOR_G;
-    extern unsigned char EDGE_ALGO_SOURCE_COLOR_B;
-    extern unsigned char EDGE_ALGO_DESTINATION_COLOR_R;
-    extern unsigned char EDGE_ALGO_DESTINATION_COLOR_G;
-    extern unsigned char EDGE_ALGO_DESTINATION_COLOR_B;
-    extern unsigned char EDGE_COLOR_R;
-    extern unsigned char EDGE_COLOR_G;
-    extern unsigned char EDGE_COLOR_B;
-    extern int           EDGE_DEFAULT_CAPACITY;
-    extern int           EDGE_MAXIMUM_CAPACITY;
-    extern int           GRAPH_MINIMUM_VERTICES;
-    extern int           GRAPH_NB_VERTICES_TRAVELING_SALESMAN;
-    extern int           GRAPH_VERTICES_INCREMENTATION;
-    extern unsigned char VERTEX_COLOR_R;
-    extern unsigned char VERTEX_COLOR_G;
-    extern unsigned char VERTEX_COLOR_B;
-    extern unsigned char VERTEX_PATH_COLOR_R;
-    extern unsigned char VERTEX_PATH_COLOR_G;
-    extern unsigned char VERTEX_PATH_COLOR_B;
-    extern unsigned char VERTEX_VISITED_COLOR_R;
-    extern unsigned char VERTEX_VISITED_COLOR_G;
-    extern unsigned char VERTEX_VISITED_COLOR_B;
+    extern const unsigned char EDGE_ALGO_RESULT_COLOR_R;
+    extern const unsigned char EDGE_ALGO_RESULT_COLOR_G;
+    extern const unsigned char EDGE_ALGO_RESULT_COLOR_B;
+    extern const unsigned char EDGE_ALGO_SOURCE_COLOR_R;
+    extern const unsigned char EDGE_ALGO_SOURCE_COLOR_G;
+    extern const unsigned char EDGE_ALGO_SOURCE_COLOR_B;
+    extern const unsigned char EDGE_ALGO_DESTINATION_COLOR_R;
+    extern const unsigned char EDGE_ALGO_DESTINATION_COLOR_G;
+    extern const unsigned char EDGE_ALGO_DESTINATION_COLOR_B;
+    extern const unsigned char EDGE_COLOR_R;
+    extern const unsigned char EDGE_COLOR_G;
+    extern const unsigned char EDGE_COLOR_B;
+    extern const int           EDGE_DEFAULT_CAPACITY;
+    extern const int           EDGE_MAXIMUM_CAPACITY;
+    extern const int           GRAPH_MINIMUM_VERTICES;
+    extern const int           GRAPH_NB_VERTICES_TRAVELING_SALESMAN;
+    extern const int           GRAPH_VERTICES_INCREMENTATION;
+    extern const unsigned char VERTEX_COLOR_R;
+    extern const unsigned char VERTEX_COLOR_G;
+    extern const unsigned char VERTEX_COLOR_B;
+    extern const unsigned char VERTEX_PATH_COLOR_R;
+    extern const unsigned char VERTEX_PATH_COLOR_G;
+    extern const unsigned char VERTEX_PATH_COLOR_B;
+    extern const unsigned char VERTEX_VISITED_COLOR_R;
+    extern const unsigned char VERTEX_VISITED_COLOR_G;
+    extern const unsigned char VERTEX_VISITED_COLOR_B;
 
 }
 
